1506a6a8504174bae67816a2baf1ac7523c92df6
ramadhan189 <fitrahrama189@gmail.com>
2025-04-12 13:11:52 +0700

    awal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import HasilSiswa from './props';
+const Awal = () => {
+  const styleCss = {
+    backgroundColor: 'lightblue'
+  }
+
+  return <div style={styleCss}>
+    <h1>makan bang!</h1>
+    <HasilSiswa/>
+    </div>
+}
+
+export { Awal }
